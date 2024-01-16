74ae4871934282d1d00420999e293e0dbed7a654
Toledo Tomás <tomas_toledo98@hotmail.com>
2024-01-16 20:52:38 -0300

    segunda pre entrega Coder

--- src/asyncMock.js
+++ src/asyncMock.js
@@ -32,7 +32,7 @@ export const getProducts = () => {
     return new Promise ((resolve)=>{
         setTimeout(()=> {
             resolve(products)
-        },500)
+        },1000)
     })
 }
 
@@ -40,14 +40,14 @@ export const getProductById = (productId) => {
     return new Promise ((resolve)=>{
         setTimeout(()=> {
             resolve(products.find(prod => prod.id === productId))
-        },500)
+        },1000)
     })
 }
 
-export const getProductsByCategory = (productCategory) => {
+export const getProductsByCategory = (categoryId) => {
     return new Promise ((resolve) => {
         setTimeout(()=> {
-            resolve(products.find(prod => prod.category === productCategory))
-        },500)
+            resolve(products.filter(prod => prod.category === categoryId))
+        },1000)
     })
 }
